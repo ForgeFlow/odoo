0065b67a508b34218b2a6b83324bb85c3fd8258e
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-04-05 12:41:41 +0200

    [FIX] sale_stock: allow to set a default warehouse using context
    We should be able to set a default warehouse  based on the company provided in the context.

--- addons/sale_stock/models/sale_order.py
+++ addons/sale_stock/models/sale_order.py
@@ -13,8 +13,8 @@ class SaleOrder(models.Model):
 
     @api.model
     def _default_warehouse_id(self):
-        company = self.env.user.company_id.id
-        warehouse_ids = self.env['stock.warehouse'].search([('company_id', '=', company)], limit=1)
+        company_id = int(self.default_get(['company_id']).get('company_id')) or self.env.user.company_id.id
+        warehouse_ids = self.env['stock.warehouse'].search([('company_id', '=', company_id)], limit=1)
         return warehouse_ids
 
     incoterm = fields.Many2one(
