67e0f82d6e9d6a3cc66f8d457192b0fe1c7ca3c0
Héctor Villarreal Ortega <hector.villarreal@forgeflow.com>
2022-03-09 19:52:53 +0100

    [HOTFIX] stock: Fix quant misalignment automatically

--- addons/stock/models/stock_quant.py
+++ addons/stock/models/stock_quant.py
@@ -476,16 +476,10 @@ class StockQuant(models.Model):
             # if we want to unreserve
             available_quantity = sum(quants.mapped('reserved_quantity'))
             if float_compare(abs(quantity), available_quantity, precision_rounding=rounding) > 0:
-                action_fix_unreserve = self.env.ref(
-                    'stock.stock_quant_stock_move_line_desynchronization', raise_if_not_found=False)
-                if action_fix_unreserve and self.user_has_groups('base.group_system'):
-                    raise RedirectWarning(
-                        _("""It is not possible to unreserve more products of %s than you have in stock.
-The correction could unreserve some operations with problematics products.""") % product_id.display_name,
-                        action_fix_unreserve.id,
-                        _('Automated action to fix it'))
-                else:
-                    raise UserError(_('It is not possible to unreserve more products of %s than you have in stock. Contact an administrator.') % product_id.display_name)
+                self._fix_quants_for_specific_product(product_id)
+                raise UserError(_('It is not possible to unreserve more products of %s than you have in stock.'
+                                  'System has tried to fix quant issue, run action again, if problem persist contact an administrator.'
+                                  ) % product_id.display_name)
         else:
             return reserved_quants
 
@@ -781,3 +775,65 @@ class QuantPackage(models.Model):
             res[quant.product_id] += quant.quantity
         return res
 
+    @api.model
+    def _fix_quants_for_specific_product(self, product_id):
+        quants = self.env['stock.quant'].search([
+            ('product_id', '=', product_id.id),
+        ])
+        move_line_ids = []
+        for quant in quants:
+            move_lines = self.env["stock.move.line"].search([
+                ('product_id', '=', quant.product_id.id),
+                ('location_id', '=', quant.location_id.id),
+                ('lot_id', '=', quant.lot_id.id),
+                ('package_id', '=', quant.package_id.id),
+                ('owner_id', '=', quant.owner_id.id),
+                ('product_qty', '!=', 0)
+            ])
+            move_line_ids += move_lines.ids
+            reserved_on_move_lines = sum(move_lines.mapped('product_qty'))
+
+            if quant.location_id.should_bypass_reservation():
+                # If a quant is in a location that should bypass the reservation, its `reserved_quantity` field
+                # should be 0.
+                if quant.reserved_quantity != 0:
+                    quant.sudo().write({'reserved_quantity': 0})
+            else:
+                # If a quant is in a reservable location, its `reserved_quantity` should be exactly the sum
+                # of the `product_qty` of all the partially_available / assigned move lines with the same
+                # characteristics.
+                if quant.reserved_quantity == 0:
+                    if move_lines:
+                        move_lines.with_context(bypass_reservation_update=True).sudo().write({'product_uom_qty': 0})
+                elif quant.reserved_quantity < 0:
+                    quant.sudo().write({'reserved_quantity': 0})
+                    if move_lines:
+                        move_lines.with_context(bypass_reservation_update=True).sudo().write({'product_uom_qty': 0})
+                else:
+                    if reserved_on_move_lines != quant.reserved_quantity:
+                        move_lines.with_context(bypass_reservation_update=True).sudo().write({'product_uom_qty': 0})
+                        quant.sudo().write({'reserved_quantity': 0})
+                    else:
+                        if any(move_line.product_qty < 0 for move_line in move_lines):
+                            move_lines.with_context(bypass_reservation_update=True).sudo().write({'product_uom_qty': 0})
+                            quant.sudo().write({'reserved_quantity': 0})
+
+        move_lines = self.env['stock.move.line'].search([
+            ('product_id', '=', product_id.id),
+            ('product_id.type', '=', 'product'),
+            ('product_qty', '!=', 0),
+            ('id', 'not in', move_line_ids),
+        ])
+
+        move_lines_to_unreserve = []
+        for move_line in move_lines:
+            if not move_line.location_id.should_bypass_reservation():
+                move_lines_to_unreserve.append(move_line.id)
+
+        if len(move_lines_to_unreserve) > 1:
+            self.env.cr.execute(""" UPDATE stock_move_line SET product_uom_qty = 0, product_qty = 0 WHERE id in %s ;""" % (
+            tuple(move_lines_to_unreserve),))
+        elif len(move_lines_to_unreserve) == 1:
+            self.env.cr.execute(""" UPDATE stock_move_line SET product_uom_qty = 0, product_qty = 0 WHERE id = %s ;""" % (
+            move_lines_to_unreserve[0]))
+        return True
