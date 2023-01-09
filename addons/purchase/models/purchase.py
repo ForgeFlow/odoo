3ed4f972a353bd15cde6694bd7ee9646c8536005
Cyril Dutrieux (cydu) <cydu@odoo.com>
2023-01-09 11:30:31 +0000

    [FIX] purchase: Prevents logging a qty update when qty isn't updated
    closes odoo/odoo#109418
    
    Signed-off-by: William Henrotin (whe) <whe@odoo.com>

--- addons/purchase/models/purchase.py
+++ addons/purchase/models/purchase.py
@@ -545,8 +545,12 @@ class PurchaseOrderLine(models.Model):
     @api.multi
     def write(self, values):
         if 'product_qty' in values:
+            precision = self.env['decimal.precision'].precision_get('Product Unit of Measure')
             for line in self:
-                if line.order_id.state == 'purchase':
+                if (
+                    line.order_id.state == "purchase"
+                    and float_compare(line.product_qty, values["product_qty"], precision_digits=precision) != 0
+                ):
                     line.order_id.message_post_with_view('purchase.track_po_line_template',
                                                          values={'line': line, 'product_qty': values['product_qty']},
                                                          subtype_id=self.env.ref('mail.mt_note').id)
